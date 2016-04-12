d86b8fc93a15119458e662f860b320395cc4776c
Attila Gulyas <toraritte@localhost.localdomain>
2016-04-11 21:13:42 -0700

    Don't wget git-prompt.sh

--- dot-install.sh
+++ dot-install.sh
@@ -1,9 +1,6 @@
 # Create aliases for the dotfiles folder
 files = ".bashrc .vimrc .bash_profile"
 
-# Needed to avoid "__git_ps1 command not found" messages for the prompt in .bashrc
-wget -O ./_work/git-prompt.sh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
-
 # Add the erl helper functions that I am "very" proud of
 cp erlang/.erlang ~
 
