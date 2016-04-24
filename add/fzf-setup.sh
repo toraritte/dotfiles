8edf8aa7c86b34c283891c26ef5b7e08d18813ba
Attila Gulyas <toraritte@gmail.com>
2016-04-23 21:09:00 -0700

    Show hidden files with :FZF in vim

--- add/fzf-setup.sh
+++ add/fzf-setup.sh
@@ -1,3 +1,7 @@
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install --all
 cp .fzf/shell/key-bindings.bash ~/.fzf/shell/
+# for the vim-fzf plugin to list hidden files as well
+# but the previous commands are still needed because
+# fzf in bash is set up differently
+export FZF_DEFAULT_COMMAND='find .'
