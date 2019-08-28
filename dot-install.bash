091fc84877fc0f908ab7ff5608d373d5ab103f87
Attila Gulyas <toraritte@gmail.com>
2019-08-28 10:13:48 -0700

    Add tmux.conf and remove kerl/exenv install

--- dot-install.bash
+++ dot-install.bash
@@ -2,7 +2,7 @@
 source $HOME/dotfiles/install-scripts/fzf-setup.bash
 
 # kerl & exenv
-source ~/dotfiles/add/erl-env-setup.bash
+# source ~/dotfiles/add/erl-env-setup.bash
 
 # link install-basic dotfiles in home to the ones in this repo
 source $HOME/dotfiles/install-scripts/dots.bash
